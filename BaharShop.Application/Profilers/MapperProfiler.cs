91dc05507b753106d8d5e0187a07ae222fd9c3c6
a.hoseni <a.hoseni@DPK-115>
2024-05-21 11:48:48 +0330

    MapperProfiler

--- BaharShop.Application/Profilers/MapperProfiler.cs
+++ BaharShop.Application/Profilers/MapperProfiler.cs
@@ -1,12 +1,22 @@
 ﻿using AutoMapper;
+using BaharShop.Application.DTOs.Addresses;
 using BaharShop.Application.DTOs.Categories;
 using BaharShop.Application.DTOs.Cities;
+using BaharShop.Application.DTOs.Comments;
 using BaharShop.Application.DTOs.Customers;
+using BaharShop.Application.DTOs.OrderItems;
+using BaharShop.Application.DTOs.Orders;
 using BaharShop.Application.DTOs.Products;
+using BaharShop.Application.DTOs.Provinces;
+using BaharShop.Domain.Entities.Addresses;
 using BaharShop.Domain.Entities.Categories;
 using BaharShop.Domain.Entities.Cities;
+using BaharShop.Domain.Entities.Comments;
 using BaharShop.Domain.Entities.Customers;
+using BaharShop.Domain.Entities.OrderItems;
+using BaharShop.Domain.Entities.Orders;
 using BaharShop.Domain.Entities.Products;
+using BaharShop.Domain.Entities.Provinces;
 
 namespace BaharShop.Application.Profilers
 {
@@ -14,10 +24,15 @@ namespace BaharShop.Application.Profilers
     {
         public MapperProfiler()
         {
-            CreateMap<Customer, CustomerDTO>().ReverseMap();
+            CreateMap<Address, AddressDTO>().ReverseMap();
             CreateMap<Category, CategoryDTO>().ReverseMap();
-            CreateMap<Product, ProductDTO>().ReverseMap();
             CreateMap<City, CityDTO>().ReverseMap();
+            CreateMap<Comment, CommentDTO>().ReverseMap();
+            CreateMap<Customer, CustomerDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDTO>().ReverseMap();
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Province, ProvinceDTO>().ReverseMap();
         }
     }
 }

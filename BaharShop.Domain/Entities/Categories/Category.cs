ba6abbf31abdab2877fcdf7c719be207c726d8a0
a.hoseni <a.hoseni@DPK-115>
2024-05-21 14:37:21 +0330

    change type of Name

--- BaharShop.Domain/Entities/Categories/Category.cs
+++ BaharShop.Domain/Entities/Categories/Category.cs
@@ -4,7 +4,7 @@ namespace BaharShop.Domain.Entities.Categories
 {
     public class Category : BaseEntity
     {
-        public int Name { get; set; }
+        public string Name { get; set; }
         public int? ParentId { get; set; }
     }
 }

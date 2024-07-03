5ad18ee1fe57cff5daeb8a6f492b1a780648e09b
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-07-03 15:06:38 +0330

    Update UserDTO.cs

--- BaharShop.Application/DTOs/Users/UserDTO.cs
+++ BaharShop.Application/DTOs/Users/UserDTO.cs
@@ -1,13 +1,15 @@
-﻿namespace BaharShop.Application.DTOs.Users
+namespace BaharShop.Application.DTOs.Users
 {
     public class UserDTO
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
 
         public string FullName { get; set; }
 
         public string Email { get; set; }
 
         public string Password { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }

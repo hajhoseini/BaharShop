03a20b21edc956fe5505fc9bb1f8fae940d64d8f
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-07-03 15:11:11 +0330

    Update UpdateUserCommandHandler.cs

--- BaharShop.Application/Features/Users/Commands/RequestHandlers/UpdateUserCommandHandler.cs
+++ BaharShop.Application/Features/Users/Commands/RequestHandlers/UpdateUserCommandHandler.cs
@@ -1,26 +1,30 @@
-﻿using MediatR;
+using MediatR;
 using AutoMapper;
 using BaharShop.Common;
 using BaharShop.Domain.IRepositories;
 using BaharShop.Domain.Entities.Users;
 using BaharShop.Application.Features.Users.Commands.Requests;
+using BaharShop.Domain.IReaders;
 
 namespace BaharShop.Application.Features.Users.Commands.RequestHandlers
 {
     public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, ResultDTO>
 	{
 		private readonly IGenericRepository<User> _genericRepository;
-		private readonly IMapper _mapper;
+        private readonly IGenericReader<User> _genericReader;
+        private readonly IMapper _mapper;
 
-		public UpdateUserCommandHandler(IGenericRepository<User> genericRepository, IMapper mapper)
+		public UpdateUserCommandHandler(IGenericRepository<User> genericRepository, IGenericReader<User> genericReader, IMapper mapper)
 		{
 			_genericRepository = genericRepository;
-			_mapper = mapper;
+            _genericReader = genericReader;
+            _mapper = mapper;
 		}
 
         public async Task<ResultDTO> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
-            var entity = _mapper.Map<User>(request.UserDTO);
+            var entity = await _genericReader.GetById(request.UserDTO.Id);
+            entity.FullName = request.UserDTO.FullName;            
             var result = await _genericRepository.Update(entity);
             return result;
         }

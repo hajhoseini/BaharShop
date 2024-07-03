19560299fddbf6ce18fed88a1686d649cb91e60d
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-07-03 15:15:10 +0330

    Update User.cs

--- BaharShop.Domain/Entities/Users/User.cs
+++ BaharShop.Domain/Entities/Users/User.cs
@@ -1,16 +1,17 @@
-﻿using BaharShop.Domain.Entities.UserRoles;
+using BaharShop.Domain.Entities.Base;
+using BaharShop.Domain.Entities.UserRoles;
 
 namespace BaharShop.Domain.Entities.Users
 {
-    public class User
+    public class User : BaseEntity
     {
-        public long Id { get; set; }
-
         public string FullName { get; set; }
 
         public string Email { get; set; }
 
         public string Password { get; set; }
+
+        public bool IsActive { get; set; }
 
         public ICollection<UserRole> UserRoles { get; set; }
     }

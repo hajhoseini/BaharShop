96c5ae1ebf5f89ba54796825bf034157b66920a5
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-07-03 15:19:36 +0330

    Update UserController.cs

--- BaharShop.WebMVC/Areas/Admin/Controllers/UserController.cs
+++ BaharShop.WebMVC/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,7 @@
+using BaharShop.Application.DTOs.Roles;
+using BaharShop.Application.DTOs.Users;
 using BaharShop.Application.Features.Roles.Queries.Requests;
+using BaharShop.Application.Features.Users.Commands.Requests;
 using BaharShop.Application.Features.Users.Queries.Requests;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +34,76 @@ namespace BaharShop.WebMVC.Areas.Admin.Controllers
 
             ViewBag.Roles = new SelectList(roles, "Id", "Name");
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string Email, string FullName, int RoleId, string Password, string RePassword)
+        {
+            var command = new RegisterUserCommand()
+            {
+                RegisterUserDTO = new RegisterUserDTO()
+                {
+                    FullName = FullName,
+                    Email = Email,
+                    Password = Password,
+                    RePassword = RePassword,
+                    roles = new List<RoleDTO>()
+                    {
+                        new RoleDTO
+                        {
+                            Id = RoleId
+                        }
+                    }
+                }
+            };
+
+            var result = await _mediator.Send(command);
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int userId)
+        {
+            DeleteUserCommand command = new DeleteUserCommand() { Id = userId };
+            var result = await _mediator.Send(command);
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserSatusChange(int userId)
+        {
+            ChangeStatusUserCommand command = new ChangeStatusUserCommand()
+            {
+                UserDTO = new UserDTO()
+                {
+                    Id = userId
+                },
+            };
+
+            var result = await _mediator.Send(command);
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int userId, string fullname)
+        {
+            UpdateUserCommand command = new UpdateUserCommand()
+            {
+                UserDTO = new UserDTO()
+                {
+                    Id = userId,
+                    FullName = fullname
+                },
+            };
+
+            var result = await _mediator.Send(command);
+            //if (result.IsSuccess)
+            //    return View("Index");
+
+            return Json(result);
         }
     }
 }

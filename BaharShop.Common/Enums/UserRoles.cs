def6995694e30935be78f58e43b9ec143a3e7da4
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-07-03 09:26:57 +0330

    Create UserRoles.cs

---
+++ BaharShop.Common/Enums/UserRoles.cs
@@ -0,0 +1,9 @@
+namespace BaharShop.Common.Enums
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Operator = "Operator";
+        public const string Customer = "Customer";
+    }
+}

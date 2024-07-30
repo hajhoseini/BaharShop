6a17133e0ff618c47c77e2f1a2e9a10d9e723dbf
a.hoseini <a.hoseini@DPK-15>
2024-07-30 15:26:22 +0330

    GetList 'HomePageImages'

---
+++ BaharShop.Application/DTOs/HomePages/HomePageImageDTO.cs
@@ -0,0 +1,15 @@
+﻿using BaharShop.Common.Enums;
+
+namespace BaharShop.Application.DTOs.HomePages
+{
+    public class HomePageImageDTO
+    {
+        public int Id { get; set; }
+
+        public string Src { get; set; }
+
+        public string Link { get; set; }
+
+        public ImageLocationEnum ImageLocation { get; set; }
+    }
+}
